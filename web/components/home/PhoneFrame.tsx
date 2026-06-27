9c9fa8573fd8817eb5cfacd54f55492d624c3593
enhhtogtoh <enhhtogtoh@gmail.com>
2026-06-27 17:47:05 +0800

    signin page hiiv

--- web/components/home/PhoneFrame.tsx
+++ web/components/home/PhoneFrame.tsx
@@ -2,11 +2,16 @@ import type { ReactNode } from "react";
 
 type PhoneFrameProps = {
   children: ReactNode;
+  className?: string;
 };
 
-export default function PhoneFrame({ children }: PhoneFrameProps) {
+export default function PhoneFrame({ children, className = "" }: PhoneFrameProps) {
+  const frameSize = className || "h-[812px] w-[390px]";
+
   return (
-    <div className="relative mx-auto h-[812px] w-[390px] max-w-[92vw] rounded-[3.3rem] bg-black p-[10px] shadow-2xl shadow-slate-900/35">
+    <div
+      className={`relative mx-auto max-w-[92vw] rounded-[3.3rem] bg-black p-[10px] shadow-2xl shadow-slate-900/35 ${frameSize}`}
+    >
       <div className="absolute left-[-3px] top-32 h-14 w-[5px] rounded-l bg-black" />
       <div className="absolute right-[-3px] top-40 h-24 w-[5px] rounded-r bg-black" />
       <div className="relative h-full overflow-hidden rounded-[2.7rem] bg-white">
